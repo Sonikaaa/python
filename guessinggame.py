37307e7d4238829bce39256f6d807f736dbf0276
Sonika Pant <105052732+Sonikaaa@users.noreply.github.com>
2024-10-02 16:36:36 +0545

    Update guessinggame.py

--- guessinggame.py
+++ guessinggame.py
@@ -5,17 +5,17 @@ out_of_guesses = False
 
 while i < guess_limit and not out_of_guesses:
     try:
-        your_number = int(input("Enter your number: "))  
+        your_number = int(input("Enter your number: "))
+        
+        if your_number == secret_number:
+            print("You won!")
+            break  
+        else:
+            i += 1
+            if i == guess_limit:
+                out_of_guesses = True
     except ValueError:
         print("Please enter a valid number!")
-        continue  # Skip the rest of the loop if input is not a number
-
-if your_number == secret_number:
-    print("You won!")
-    break  # Exit the loop if the guess is correct
-else:
-    i += 1
-    if i == guess_limit:
-        out_of_guesses = True
+        
 if out_of_guesses:
     print("Sorry, no guesses left!")
